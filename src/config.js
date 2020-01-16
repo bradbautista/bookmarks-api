8099c3e627c5cfc2936b28b9386ddb993102b2ad
unknown <bradbautista@gmail.com>
2020-01-15 20:12:55 -0500

    preparing to deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:password@localhost/bookmarks',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:password@localhost/bookmarks',
   API_TOKEN: 'TOKEN_NOT_SET'
 }
